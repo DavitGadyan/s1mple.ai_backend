97b4b5d4e4e4c1d9d681348a3fe12b6c7b90364c
Davit Gadyan <dyada@MSI.>
2025-07-06 00:53:05 +0200

    cors added

--- main_cors.py
+++ main_cors.py
@@ -14,6 +14,7 @@ app.add_middleware(
         "http://localhost:8080",  # if you use a different port
         "http://localhost:8000",  # if you use a different port
         "https://api.s1mpleai.org",
+        "https://s1mpleai.org",
         "https://s1mple-ai-2025-hc4l7mer5-davitgadyans-projects.vercel.app/",
     ],
     allow_credentials=True,
